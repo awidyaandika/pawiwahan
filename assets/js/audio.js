5957e7b8e11b35e87d566e4982c54e5a6819f0c8
awidyaandika <awidyaandika@gmail.com>
2020-12-22 21:55:12 +0800

    v1.5.2
    tambah music tp blm fix di button

---
+++ assets/js/audio.js
@@ -0,0 +1,9 @@
+if($(".music-box").length) {
+    var musicBtn = $(".music-box-toggle-btn"),
+        musicBox = $(".music-holder");
+
+    musicBtn.on("click", function() {
+        musicBox.toggleClass("toggle-music-box");
+        return false;
+    })
+}
